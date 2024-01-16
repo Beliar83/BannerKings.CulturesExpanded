99fba8852511b28cf74de2b67e77c65f25adb27f
rodrigo <rodrigo.v.melo@hotmail.com>
2024-01-16 20:14:26 +0000

    Cavalry debuffs for sailing party

---
+++ BannerKings.TroopOverhaul/Models/BKCEBattleSimulationModel.cs
@@ -0,0 +1,32 @@
+﻿using BannerKings.Models.Vanilla;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.MapEvents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+
+namespace BannerKings.CulturesExpanded.Models
+{
+    public class BKCEBattleSimulationModel : BKBattleSimulationModel
+    {
+        public override int SimulateHit(CharacterObject strikerTroop, CharacterObject struckTroop, PartyBase strikerParty, PartyBase struckParty, float strikerAdvantage, MapEvent battle)
+        {
+            float result = base.SimulateHit(strikerTroop, struckTroop, strikerParty, struckParty, strikerAdvantage, battle);
+
+            if (strikerParty.IsMobile && TaleWorlds.CampaignSystem.Campaign.Current.MapSceneWrapper
+                .GetFaceTerrainType(strikerParty.MobileParty.CurrentNavigationFace) == TerrainType.Swamp)
+            {
+                if (strikerTroop.IsMounted)
+                {
+                    result *= 0.6f;
+                }
+
+                if (struckTroop.IsMounted)
+                {
+                    result = 1.2f;
+                }
+            }
+
+            return (int)result;
+        }
+    }
+}
---
+++ BannerKings.TroopOverhaul/Models/BKCEPartyFoodModel.cs
@@ -0,0 +1,34 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+
+namespace BannerKings.CulturesExpanded.Models
+{
+    public class BKCEPartyFoodModel : DefaultMobilePartyFoodConsumptionModel
+    {
+        public override ExplainedNumber CalculateDailyFoodConsumptionf(MobileParty party, ExplainedNumber baseConsumption)
+        {
+            ExplainedNumber result = base.CalculateDailyFoodConsumptionf(party, baseConsumption);
+            if (TaleWorlds.CampaignSystem.Campaign.Current.MapSceneWrapper.GetFaceTerrainType(party.CurrentNavigationFace) == TerrainType.Swamp)
+            {
+                float mounts = party.ItemRoster.NumberOfMounts + party.ItemRoster.NumberOfLivestockAnimals +
+                    party.ItemRoster.NumberOfPackAnimals;
+                foreach (var troop in party.MemberRoster.GetTroopRoster())
+                {
+                    if (troop.Character.IsMounted) mounts++;
+                }
+
+                foreach (var troop in party.PrisonRoster.GetTroopRoster())
+                {
+                    if (troop.Character.IsMounted) mounts++;
+                }
+
+                result.Add(mounts / 2f, new TaleWorlds.Localization.TextObject("{=!}Carrying {COUNT} animals while sailing (inventory, party and prisoners)")
+                    .SetTextVariable("COUNT", mounts.ToString("0")));
+            }
+            
+            return result;
+        }
+    }
+}
