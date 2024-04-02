d6fa5213d69a01c97c1fcec17f6d89bbb396424c
rodrigo <rodrigo.v.melo@hotmail.com>
2024-04-02 11:39:08 +0100

    Models

---
+++ BannerKings.TroopOverhaul/Models/BKCEEquipmentModel.cs
@@ -0,0 +1,31 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Extensions;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace BannerKings.CulturesExpanded.Models
+{
+    public class BKCEEquipmentModel : DefaultEquipmentSelectionModel
+    {
+        public override MBList<MBEquipmentRoster> GetEquipmentRostersForHeroComeOfAge(Hero hero, bool isCivilian)
+        {
+            MBList<MBEquipmentRoster> result = base.GetEquipmentRostersForHeroComeOfAge(hero, isCivilian);
+            if (result.IsEmpty())
+            {
+                foreach (MBEquipmentRoster mbequipmentRoster in MBEquipmentRosterExtensions.All)
+                {
+                    if (mbequipmentRoster.EquipmentCulture == hero.Culture)
+                    {
+                        if (!isCivilian && mbequipmentRoster.HasEquipmentFlags(EquipmentFlags.IsCombatantTemplate))
+                            result.Add(mbequipmentRoster);
+                        else if (isCivilian && mbequipmentRoster.HasEquipmentFlags(EquipmentFlags.IsCivilianTemplate))
+                            result.Add(mbequipmentRoster);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- BannerKings.TroopOverhaul/Models/BKCEPartyFoodModel.cs
+++ BannerKings.TroopOverhaul/Models/BKCEPartyFoodModel.cs
@@ -12,7 +12,7 @@ namespace BannerKings.CulturesExpanded.Models
             ExplainedNumber result = base.CalculateDailyFoodConsumptionf(party, baseConsumption);
             if (TaleWorlds.CampaignSystem.Campaign.Current.MapSceneWrapper.GetFaceTerrainType(party.CurrentNavigationFace) == TerrainType.Swamp)
             {
-                float mounts = CalculateAnimalFoodNeed(party);
+                float mounts = CalculateAnimalFoodNeed(party, false);
                 result.Add(mounts / 2f, new TaleWorlds.Localization.TextObject("{=!}Carrying {COUNT} animals while sailing (inventory, party and prisoners)")
                     .SetTextVariable("COUNT", mounts.ToString("0")));
             }
