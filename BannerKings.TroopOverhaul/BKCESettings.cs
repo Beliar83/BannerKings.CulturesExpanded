338988d11c0cfda3bd135c0c4bca784245540b28
rodrigo <rodrigo.v.melo@hotmail.com>
2024-04-02 11:35:01 +0100

    Updated settings

--- BannerKings.TroopOverhaul/BKCESettings.cs
+++ BannerKings.TroopOverhaul/BKCESettings.cs
@@ -4,15 +4,17 @@ using MCM.Abstractions.Base.Global;
 
 namespace BannerKings.Settings
 {
-    public class BKTOSettings : AttributeGlobalSettings<BKTOSettings>
+    public class BKCESettings : AttributeGlobalSettings<BKCESettings>
     {
-        private int volunteersLimit = 10;
         public override string Id => "BannerKings.CulturesExpanded";
         public override string DisplayName => new TextObject("{=!}Banner Kings: Cultures Expanded").ToString();
         public override string FolderName => "BannerKings.CulturesExpanded"; 
         public override string FormatType => "json2";
 
-        [SettingProperty("{=!}Latin Titles", RequireRestart = true, HintText = "{=!}Switch hellenized Empire titles for Latin language titles. Default: False.")]
+        [SettingProperty("{=!}Imperial Latin Titles", RequireRestart = true, HintText = "{=!}For Empire titles, switch the use of Hellenic titles to Latin titles. Default: False.")]
         public bool LatinTitles { get; set; } = false;
+
+        [SettingProperty("{=!}Battanian Goidelic Titles", RequireRestart = true, HintText = "{=!}For Battanian titles, switch the use of Brythonic titles to Goedelic titles. Default: False.")]
+        public bool GoidelicTitles { get; set; } = false;
     }
 }
